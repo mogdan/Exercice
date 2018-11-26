03b5964cf53da0a87f5165f6a56f0bf428176354
mogdan <phan_daniel@ymail.com>
2018-11-26 09:07:08 +0100

    - integration to intelliJ Idea - add main class : to play game - add Game class

---
+++ src/main/Game.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Game {
+    String name;
+
+    public void play(){
+        System.out.println("Vous jouez au jeu "+name);
+    }
+}
---
+++ src/main/main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class main {
+    public static void main(String[] args) {
+        Game gow = new Game();
+        gow.name = "God of War";
+        gow.play();
+    }
+
+}
